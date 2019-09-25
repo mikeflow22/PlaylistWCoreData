ab829ba480c709bf7421ab234b6415ff55095d77
Michael R.F <mrflowers22@uwalumni.com>
2019-09-25 12:14:40 -0600

    completed the playlistController instance

--- PlaylistWCoreData/PlaylistWCoreData/ModelController/PlaylistController.swift
+++ PlaylistWCoreData/PlaylistWCoreData/ModelController/PlaylistController.swift
@@ -11,12 +11,22 @@ import CoreData
 
 class PlaylistController {
     
+    static let shared = PlaylistController()
+    
+    //local source of truth - do you need this if you are using nsfetchRequstController?
+    var playlists: [Playlist] {
+        let fetchRequest: NSFetchRequest<Playlist> = Playlist.fetchRequest()
+        return (try? CoreDataStack.mainContext.fetch(fetchRequest)) ?? []
+    }
+    
     func createPlaylist(withName name: String){
-
+         _ = Playlist(playlistName: name)
+        saveToPersistentStore()
     }
     
     func deletePlaylist(playlist: Playlist){
-        
+        CoreDataStack.mainContext.delete(playlist)
+        saveToPersistentStore()
     }
     
     func saveToPersistentStore(){
@@ -26,8 +36,5 @@ class PlaylistController {
             print("Error saving to mainContext: \(error) in \(#function)")
         }
     }
-    
-//    func loadFromPersistentStore(){
-//
-//    }
+
 }
