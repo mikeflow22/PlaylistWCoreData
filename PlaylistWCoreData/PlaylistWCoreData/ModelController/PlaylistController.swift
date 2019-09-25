93fe11bd43f33b58be33ff3191c53cd59af2e762
Michael R.F <mrflowers22@uwalumni.com>
2019-09-25 11:47:52 -0600

    created playlist controller file

---
+++ PlaylistWCoreData/PlaylistWCoreData/ModelController/PlaylistController.swift
@@ -0,0 +1,14 @@
+//
+//  PlaylistController.swift
+//  PlaylistWCoreData
+//
+//  Created by Michael Flowers on 9/25/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class PlaylistController {
+    
+}
